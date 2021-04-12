38ba5142259877f11eb63d1bca425bdd859ebfe4
Adrian Hitos <adrian@MacBook-Pro-de-admin.local>
2021-04-12 15:31:23 +0200

    all separated in modules, doesn't work

---
+++ context/book/primary-adapter/src/main/rest/spring-web-service/src/BooksController.kt
@@ -0,0 +1,26 @@
+package si.dime.kotlin.tutorials.rest.booklibrary.book.primaryAdapter
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import si.dime.kotlin.tutorials.rest.booklibrary.book.domain.src.main.kotlin.Book
+import si.dime.kotlin.tutorials.rest.booklibrary.book.secondaryAdapter.composite.InMemoryBooksDatabase
+import si.dime.kotlin.tutorials.rest.booklibrary.book.domain.DuplicateItemException
+
+@RestController
+class BooksController {
+
+    // Our books' database
+    @Autowired
+    private lateinit var database: InMemoryBooksDatabase
+
+    @RequestMapping("/books", method = arrayOf(RequestMethod.GET))
+    fun books() = database.getBooks()
+
+    @RequestMapping("/book", method = [RequestMethod.POST])
+    fun addBook(@RequestBody book: Book) =
+        if (database.addBook(book)) book
+        else throw DuplicateItemException()
+}
