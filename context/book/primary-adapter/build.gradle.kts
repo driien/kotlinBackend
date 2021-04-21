1c7369053f3f0e04f0c1a627e060f8adfab66904
Alex <alex.amenos@code.seat>
2021-04-22 00:00:56 +0200

    Fix package problem, dependecies and best practices

--- context/book/primary-adapter/build.gradle.kts
+++ context/book/primary-adapter/build.gradle.kts
@@ -3,5 +3,11 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":context:book:domain"))
+
     implementation("org.springframework.boot:spring-boot-starter-web:2.3.3.RELEASE")
+    implementation("org.axonframework:axon-spring-boot-starter:4.3.2") {
+        exclude(group = "org.axonframework", module = "axon-server-connector")
+    }
+    implementation("org.axonframework.extensions.tracing:axon-tracing-spring-boot-starter:4.3")
 }
