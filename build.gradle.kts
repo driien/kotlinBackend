e79c422c91e3fad3bd8796f69689d5eef44c9fbf
Adrian Hitos <adrian@MacBook-Pro-de-admin.local>
2021-04-23 12:21:53 +0200

    buildgradle modified to add jvm version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.4.32"
     id("org.springframework.boot") version "2.3.3.RELEASE"
@@ -13,33 +15,32 @@ allprojects {
         maven { setUrl ("https://jitpack.io")}
     }
 
+    java.sourceCompatibility = JavaVersion.VERSION_11
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
     dependencies {
         implementation("javax.inject:javax.inject:1")
         implementation("org.springframework.boot:spring-boot-starter-web:2.3.3.RELEASE")
 
     }
-
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-
-    //JavaX
     implementation("javax.transaction:javax.transaction-api:1.3")
-
-    //Axon framework
     implementation("org.axonframework:axon-spring-boot-starter:4.3.2") {
         exclude(group = "org.axonframework", module = "axon-server-connector")
     }
     implementation("org.axonframework.extensions.tracing:axon-tracing-spring-boot-starter:4.3")
-
-    //CQRS
     implementation("com.github.kotato:KAxon-CQRS:0.0.10")
-
-    //Test
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-
-
 }
-
